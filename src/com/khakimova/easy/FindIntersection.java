ea9c61f365b73060e203a5b7abd1f1b7355e1ff6
Asiya Khakimova <asiya.khakimova@simbirsoft.com>
2022-03-16 21:58:09 +0300

    findIntersection2

--- src/com/khakimova/easy/FindIntersection.java
+++ src/com/khakimova/easy/FindIntersection.java
@@ -1,6 +1,12 @@
 package com.khakimova.easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,7 +36,6 @@ public class FindIntersection {
         Integer[] numbers2 = convertToArrayOfInteger(strArr[1]);
 
         int j = 0;
-
         for (Integer number : numbers1) {
             while (j < numbers2.length && number >= numbers2[j]) {
                 if (Objects.equals(number, numbers2[j])) {
@@ -47,12 +52,28 @@ public class FindIntersection {
         return intersection.substring(0, intersection.length()-1);
     }
 
+    private String findIntersection2(String[] strArr) {
+        List<String> intersection = new ArrayList<>();
+        List<List<String>> arrays = Arrays.stream(strArr).map(str -> Arrays.asList(str.split(", "))).collect(Collectors.toList());
+
+        arrays.get(0).forEach( element -> {
+            if(arrays.get(1).contains(element)) {
+                intersection.add(element);
+            }
+        });
+
+        if(intersection.isEmpty()) {
+            return "false";
+        }
+        return intersection.stream().collect(Collectors.joining(","));
+    }
+
     private Integer[] convertToArrayOfInteger(String str) {
-        String[] strings = str.split(",");
+        String[] strings = str.split(", ");
         Integer[] numbers = new Integer[strings.length];
 
         for(int i=0; i < strings.length; i++) {
-            numbers[i] = Integer.parseInt(strings[i].trim());
+            numbers[i] = Integer.parseInt(strings[i]);
         }
 
         return numbers;
@@ -62,5 +83,11 @@ public class FindIntersection {
     public void findIntersectionTest() {
         assertEquals("1,4,13", findIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
         assertEquals("1,9,10", findIntersection(new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"}));
+    }
+
+    @Test
+    public void findIntersection2Test() {
+        assertEquals("1,4,13", findIntersection2(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+        assertEquals("1,9,10", findIntersection2(new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"}));
     }
 }
