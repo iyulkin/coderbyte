28046157198dcfc3c22048db7267be210cfeb83a
Asiya Khakimova <asiya.khakimova@simbirsoft.com>
2022-03-16 21:40:14 +0300

    findIntersection

---
+++ src/com/khakimova/easy/FindIntersection.java
@@ -0,0 +1,66 @@
+package com.khakimova.easy;
+
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Have the function FindIntersection(strArr) read the array of strings stored in strArr
+ * which will contain 2 elements:
+ * the first element will represent a list of comma-separated numbers sorted in ascending order,
+ * the second element will represent a second list of comma-separated numbers (also sorted).
+ *
+ * Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order.
+ * If there is no intersection, return the string false.
+ *
+ * Examples
+ * Input: new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}
+ * Output: 1,4,13
+ *
+ * Input: new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"}
+ * Output: 1,9,10
+ */
+public class FindIntersection {
+
+    private String findIntersection(String[] strArr) {
+        String intersection = "";
+
+        Integer[] numbers1 = convertToArrayOfInteger(strArr[0]);
+        Integer[] numbers2 = convertToArrayOfInteger(strArr[1]);
+
+        int j = 0;
+
+        for (Integer number : numbers1) {
+            while (j < numbers2.length && number >= numbers2[j]) {
+                if (Objects.equals(number, numbers2[j])) {
+                    intersection = String.format("%s%d,", intersection, number);
+                }
+                j++;
+            }
+        }
+
+        if(intersection.isBlank()) {
+            return "false";
+        }
+
+        return intersection.substring(0, intersection.length()-1);
+    }
+
+    private Integer[] convertToArrayOfInteger(String str) {
+        String[] strings = str.split(",");
+        Integer[] numbers = new Integer[strings.length];
+
+        for(int i=0; i < strings.length; i++) {
+            numbers[i] = Integer.parseInt(strings[i].trim());
+        }
+
+        return numbers;
+    }
+
+    @Test
+    public void findIntersectionTest() {
+        assertEquals("1,4,13", findIntersection(new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}));
+        assertEquals("1,9,10", findIntersection(new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"}));
+    }
+}
